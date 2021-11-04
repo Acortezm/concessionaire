45c9e36c47c35b43fe6bedf68d7b54e1b4119520
Johnatan Agudelo Múnera <johnatan.fisica@gmail.com>
2021-11-04 11:30:44 -0500

    Agregar archivo EditProduct para editar un producto previamente agregado a la base de datos y agregar el link correspondiente en el boton Editar de la tabla

--- src/pages/Admin/Productos.jsx
+++ src/pages/Admin/Productos.jsx
@@ -3,82 +3,66 @@ import { Link } from 'react-router-dom';
 import BotonExtra from '../../componentes/botones/BotonExtra';
 
 
-
-
-
 const Productos = () => {
-    const productoBackend = [
-        {
-            nombre: "hueso",
-            ID: "1234",
-            precio: "13.00",
-            disponibilidad: Boolean,
-        },
-
-        {
-            nombre: "hueso",
-            ID: "1234",
-            precio: "13.00",
-            disponibilidad: Boolean,
-        }
-
-
-    ]
+    
     return (
         <div className='absolute inset-y-0 right-32'>
             <div className="border-b pb-6 border-blue-400 mt-16">
                 <h1 className="flex items-center md:mt-0 mt-4" role="heading" aria-label="profile information" className="focus:outline-none text-3xl font-bold text-gray-800">Administración de Productos
                 </h1>
             </div>
-            <div className="mt-10 ml-0">
-                <Link to="/Admin/AddProduct"> < BotonExtra > Agregar Producto </BotonExtra></Link></div>
+            <div className="flex items-center justify-center">
+                <Link to="/Admin/AddProduct"> < BotonExtra > Agregar Producto </BotonExtra></Link>
+                < BotonExtra > Lista de productos </BotonExtra>
+            </div>
+                
             <table className=' mt-10'>
                 <thead>
                     <tr>
-                        <th class="bg-gray-50 border-b p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
-                            <div class="flex items-center justify-center">
+                        <th className="bg-gray-50 border-b p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                            <div className="flex items-center justify-center">
                                 ID del Producto
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
                                 </svg>
                             </div>
                         </th>
-                        <th class=" bg-gray-50 border-b p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
-                            <div class="flex items-center justify-center">
+                        <th className=" bg-gray-50 border-b p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                            <div className="flex items-center justify-center">
                                 Producto
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
                                 </svg>
                             </div>
                         </th>
-                        <th class="bg-gray-50 border-b p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
-                            <div class="flex items-center justify-center">
+                        <th className="bg-gray-50 border-b p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                            <div className="flex items-center justify-center">
                                 Precio
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
                                 </svg>
                             </div>
                         </th>
-                        <th class="bg-gray-50 border-b  p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
-                            <div class="flex items-center justify-center">
+                        <th className="bg-gray-50 border-b  p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                            <div className="flex items-center justify-center">
                                 Disponibilidad
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
                                 </svg>
                             </div>
                         </th>
-                        <th class="bg-gray-50 border-b  p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
-                            <div class="flex items-center justify-center">
-                                Actualizar
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <th className="bg-gray-50 border-b  p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                            <div className="flex items-center justify-center">
+                                Editar
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
                                 </svg>
                             </div>
                         </th>
-                        <th class="bg-gray-50 border-b  p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
-                            <div class="flex items-center justify-center">
+                        <th className="bg-gray-50 border-b  p-2 border-r cursor-pointer text-sm font-thin text-gray-500">
+                            <div className="flex items-center justify-center">
                                 Eliminar
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
                                 </svg>
 
@@ -96,16 +80,17 @@ const Productos = () => {
                     <td>
                     </td>
                     <td>
-                        <div class="flex items-center justify-center">
-                            <button className="bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-1 
-        focus:ring-gray-700 py-2 px-2 border rounded-lg border-gray-200 flex items-center w-8 font text-white justify-center"><i class="far fa-edit"></i></button>
+                        <div className="flex items-center justify-center">
+                         
+                         <Link to="/Admin/EditProduct"><button className="bg-green-500 focus:outline-none focus:ring-2 focus:ring-offset-1 
+        focus:ring-gray-700 py-2 px-2 border rounded-lg border-gray-200 flex items-center w-8 font text-white justify-center"><i className="far fa-edit"></i></button></Link>
                         </div>
                     </td>
 
                     <td>
-                        <div class="flex items-center justify-center">
+                        <div className="flex items-center justify-center">
                             <button className="bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-1 
-        focus:ring-gray-700 py-2 px-2 border rounded-lg border-gray-200 flex items-center w-8 font text-white justify-center" ><i class="far fa-trash-alt"></i></button>
+        focus:ring-gray-700 py-2 px-2 border rounded-lg border-gray-200 flex items-center w-8 font text-white justify-center" ><i className="far fa-trash-alt"></i></button>
                         </div>
                     </td>
 
