190f8dbf794c6463be7cd6f8b3cc6e10813052ce
Johnatan Agudelo Múnera <johnatan.fisica@gmail.com>
2021-11-13 14:47:08 -0500

    Despliegue con Heroku Test

--- server.js
+++ server.js
@@ -14,15 +14,32 @@ app.use(express.json());
 
 //Configurar la conexion en el mongo atlas
 const mongoose = require('mongoose');
-const URI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.tazd8.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`
-const option = {useNewUrlParser: true, useUnifiedTopology: true }; 
+//const URI = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.tazd8.mongodb.net/${process.env.DATABASE}?retryWrites=true&w=majority`
+//const option = {useNewUrlParser: true, useUnifiedTopology: true }; 
 mongoose.connect(URI, option)
-.then(() => {
-    console.log("Base de datos conectada")
- })
-.catch((e) => console.log("Error en la conexión: " + e));
+//.then(() => {
+//    console.log("Base de datos conectada")
+// })
+//.catch((e) => console.log("Error en la conexión: " + e));
 
-if(process.env.NODE_ENV==='production'){
+//if(process.env.NODE_ENV==='production'){
+///    app.use(express.static('frontend/build'));
+//}
+
+const app = express();
+const PORT = process.env.PORT || 8080; // Step 1
+
+// Step 2
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/myFirstDatabase', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
+});
+
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
 }
 
@@ -33,7 +50,9 @@ const {ventas_routes} = require('./routes')
 app.use('/api/v1/ventas', ventas_routes);
 const {usuarios_routes} = require('./routes')
 app.use('/api/v1/usuarios', usuarios_routes);
+
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
     
 //Poner el servidor a escuchar
-app.listen(process.env.PORT || 8080, () => {console.log("Servidor funcionando en el puerto: "+process.env.PORT)})
+//app.listen(process.env.PORT || 8080, () => {console.log("Servidor funcionando en el puerto: "+process.env.PORT)})
 
